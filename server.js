f57a5dc259c5acffcbe27a2bcac30bbc5ed6ce49
priyankabhesaniya <priyanka.b@wingsts.com>
2024-10-01 00:23:49 +0530

    nothing

--- server.js
+++ server.js
@@ -1,67 +1,3 @@
-// const jsonServer = require('json-server');
-// const auth = require('json-server-auth');
-// const bcrypt = require('bcrypt');
-// const jwt = require('jsonwebtoken');
-// const cors = require('cors'); 
-// const app = jsonServer.create();
-// const router = jsonServer.router('db.json');
-
-// // Secret key for JWT
-// const SECRET_KEY = 'your-secret-key';
-// app.use(jsonServer.bodyParser)
-// app.use(jsonServer.defaults());
-// app.use(cors())
-// app.post('/custom-login', async (req, res) => {
-//   const { email, password, role } = req.body;
-// const user = router.db.get('users').find({ email }).value();
-  
-//   console.log("🚀 ~ app.post ~ user:", user)
-//   // Check if the user exists
-//   if (!user) {
-//     return res.status(401).json({ error: 'User not found with this mail' });
-//   }
-//   const isMatch = await bcrypt.compare(password, user.password);
-//   if (!isMatch) {
-//     return res.status(401).json({ error: 'Invalid email or password' });
-//   }
-//   if(user && user.role !== role){
-//     return res.status(401).json({ error: 'Please enter correct role' });
-//   }
-
-//   // Password comparison based on the role
-//   // if (role === 'Admin') {
-//   //   // For Admin, we compare the encrypted password
-//   //   const isMatch = await bcrypt.compare(password, user.password);
-//     // if (!isMatch) {
-//     //   return res.status(401).json({ error: 'Invalid email or password' });
-//     // }
-//   // } else {
-//   //   // For Manager and Employee, directly compare the plain-text password
-//     // if (user.password !== password) {
-//     //   return res.status(401).json({ error: 'Invalid email or password' });
-//     // }
-//   // }
-
-//   // Generate JWT token
-//   const token = jwt.sign(
-//     { id: user.id, email: user.email, role: user.role },
-//     SECRET_KEY,
-//     { expiresIn: '1m' }
-//   );
-
-//   // Return the token and user data
-//   return res.status(200).json({ token, user });
-// });
- 
-// app.db = router.db;
-
-// app.use(auth);
-// app.use(router);
-
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => {
-//   console.log(`JSON Server running on http://localhost:${PORT}`);
-// });
 const jsonServer = require('json-server');
 const auth = require('json-server-auth');
 const bcrypt = require('bcrypt');
@@ -71,7 +7,6 @@ const app = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 
-// Secret key for JWT
 const SECRET_KEY = 'mnbvcxz';
 
 // Apply middlewares (CORS, static files, logging, etc.)
